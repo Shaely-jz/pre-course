2a837e2159b69e7cdd4bd7d76eee3381ebade8b2
Shaely <shaely@km.ru>
2020-05-13 18:46:25 +0300

    ​Новый файл Numbers и новый цикл в main

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-name = input('Кто создатель Python? ')
+# Задавать вопросы, пока пользователь не введёт верный ответ
+name = input('Имя создателя Python: ')
 
 while name != 'Гвидо':
     print('Неверно')
-    name = input('Кто создатель Python? ')
+    name = input('Имя создателя Python: ')
+
+print('Верно')
+
+# Вариант с использованием break
+
+name = None
+
+while True:
+    name = input('Имя создателя Linux: ')
+    if name == 'Линус':
+        break
+    print('Неверно')
 
 print('Верно')
---
+++ numbers 0-n.py
@@ -0,0 +1,8 @@
+# Вывести цифры от 0 до n
+
+number = 0
+n = int(input('Введите n: '))
+
+while number <= n:
+    print (number)
+    number += 1
